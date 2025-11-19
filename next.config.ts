70a1a7a2ed90349666fb1375a13476af14d19db2
Mohammad Emami <162024353+mohammademami2005@users.noreply.github.com>
2025-11-20 03:06:27 +0330

    add manifest and config pwa and config metadata

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,53 @@
 import type { NextConfig } from "next";
 
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  buildExcludes: [/middleware-manifest\.json$/],
+  runtimeCaching: [
+    // فایل‌های استاتیک Next.js
+    {
+      urlPattern: /^https:\/\/radio-javan\.mohammademamiproject\.ir\/_next\/static\//,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "static-resources",
+        expiration: {
+          maxEntries: 50,
+          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 روز
+        },
+      },
+    },
+    {
+      urlPattern: /^https:\/\/radio-javan\.mohammademamiproject\.ir\/images\//,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "image-cache",
+        expiration: {
+          maxEntries: 60,
+          maxAgeSeconds: 60 * 24 * 60 * 60, // 60 روز
+        },
+      },
+    },
+    // API
+    {
+      urlPattern: /^https:\/\/radio-javan\.mohammademamiproject\.ir\/api\//,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "api-cache",
+        networkTimeoutSeconds: 10,
+        expiration: {
+          maxEntries: 30,
+          maxAgeSeconds: 24 * 60 * 60, // 1 روز
+        },
+      },
+    },
+  ],
+});
+
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true
+  reactCompiler: true,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
